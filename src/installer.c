cf1d3a58b4b0c2f7e19a6e2ebded8a973fc02f03
RedBees <redbeesraae@gmail.com>
2023-02-20 01:57:07 -0500

    Fixed error handling

--- src/installer.c
+++ src/installer.c
@@ -21,6 +21,28 @@
 #define NANDBOOT_PAYLOAD_PATH "/boot2/payload.bin"
 #define BOOT2WAD_PATH         "/boot2/boot2.wad"
 
+#define INSTALL_SD_BOOT 0
+#define INSTALL_NAND_BOOT 1
+#define INSTALL_WAD 2
+
+void HandleInstall(s32 ret, u8 installType) {
+	switch(ret){
+		case 0:
+			printf("%s was installed successfully!\n", (installType == INSTALL_SD_BOOT) ? "SDBoot" : "NANDBoot"); break;
+			if(installType == INSTALL_WAD) { printf("boot2 WAD was installed successfully!\n"); break;}
+		case MISSING_FILE:
+			ThrowError(errorStrings[ErrStr_MissingFiles]); break;
+		case BOOT2_DOLPHIN:
+			ThrowError(errorStrings[ErrStr_InDolphin]); break;
+		case HASH_MISMATCH:
+			ThrowError(errorStrings[ErrStr_BadFile]); break;
+		case CANNOT_DOWNGRADE: // TODO: Find out why this triggers sometimes even after writing SEEPROM
+			printf("Error: cannot downgrade boot2\n"); break;
+		default:
+			ThrowErrorEx(errorStrings[ErrStr_Generic], ret); break;
+	}
+}
+
 void SEEPROMClearStep( void ) {
 	printf("\n\nPress any controller button to clear the boot2 version.");
 	WaitForPad();
@@ -41,48 +63,24 @@ void SDBootInstaller( void ) {
 		printf("Press any controller button to continue anyways or press the power button on the console to exit.\n"); // TODO: Add SD file check
 		WaitForPad();																			
 		}
-	s32 ret = 0;
-	ret = InstallSDBoot(SDBOOT_PATH);
 	
+	HandleInstall(InstallSDBoot(SDBOOT_PATH), INSTALL_SD_BOOT);
 	printf("\nPress any button to continue.");
 	WaitForPad();
 }
 
 void NANDBootInstaller( void ) {
 	SEEPROMClearStep();
-	s32 ret = 0;
-	ret = InstallNANDBoot(NANDBOOT_PATH, NANDBOOT_PAYLOAD_PATH);
-	
+
+	HandleInstall(InstallNANDBoot(NANDBOOT_PATH, NANDBOOT_PAYLOAD_PATH), INSTALL_NAND_BOOT);	
 	printf("\nPress any button to continue.");
 	WaitForPad();
 }
 
-void Boot2WADInstaller( void ) {
-	s32 ret = 0;
-	
+void Boot2WADInstaller( void ) {	
 	printf("\n\n");
-	InstallWADBoot2(BOOT2WAD_PATH);
-	
+
+	HandleInstall(InstallWADBoot2(BOOT2WAD_PATH), INSTALL_WAD);	
 	printf("\nPress any button to continue.");
 	WaitForPad();	
 }
-
-/*	switch(ret){
-		case 0:
-			if(choice & WPAD_BUTTON_PLUS) {printf("boot2 WAD was installed successfully!\n"); break;}
-			printf("%s was installed successfully!\n", (choice & WPAD_BUTTON_A) ? "SDBoot" : "NANDBoot"); break;
-		case MISSING_FILE:
-			ThrowError(errorStrings[ErrStr_MissingFiles]); break;
-		case BOOT2_DOLPHIN:
-			ThrowError(errorStrings[ErrStr_InDolphin]); break;
-		case HASH_MISMATCH:
-			ThrowError(errorStrings[ErrStr_BadFile]); break;
-		case CANNOT_DOWNGRADE: // TODO: Find out why this triggers sometimes even after writing SEEPROM
-			printf("Error: cannot downgrade boot2\n"); break;
-		default:
-			ThrowErrorEx(errorStrings[ErrStr_Generic], ret); break;
-	}*/
-
-/*	WaitExit();
-	return 0;	// NOT REACHED HERE
-}*/
